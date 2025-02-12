98f4a8fb25549cc899a167bb42a01c49f911bc90
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-12 15:29:05 -0500

    entity creation works!

--- P1DemoBackend/src/main/java/com/revature/P1DemoBackend/P1DemoBackendApplication.java
+++ P1DemoBackend/src/main/java/com/revature/P1DemoBackend/P1DemoBackendApplication.java
@@ -2,9 +2,14 @@ package com.revature.P1DemoBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("com.revature.models") //Tells spring to scan this package for DB entities
+@ComponentScan("com.revature") //Tells spring to scan this package for beans
 public class P1DemoBackendApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(P1DemoBackendApplication.class, args);
--- P1DemoBackend/src/main/java/com/revature/models/User.java
+++ P1DemoBackend/src/main/java/com/revature/models/User.java
@@ -76,4 +76,5 @@ public class User {
                 ", role='" + role + '\'' +
                 '}';
     }
+
 }
