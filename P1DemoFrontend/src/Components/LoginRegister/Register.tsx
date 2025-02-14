833cf74165a63443d42b708b86c29d3367854983
Acohen455 <aaroncohen455@gmail.com>
2025-02-14 13:29:52 -0700

    updates

--- P1DemoFrontend/src/Components/LoginRegister/Register.tsx
+++ P1DemoFrontend/src/Components/LoginRegister/Register.tsx
@@ -1,8 +1,23 @@
-import {Container, Form} from "react-bootstrap";
+import {Button, Container, Form} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import * as axios from "axios";
 
 
 export const Register:React.FC = () => {
 
+
+    //TODO: gather user input like with hypot calculator
+
+    //Hardcoding a new user registration with axios
+    //axios is a way to send HTTP requests from React
+    const register = async () => {
+        //POST request with hardcoded user info
+        //axios takes the URL we're going to send to, the data we're sending (if any), and the config
+        const response = await axios.post("http://localhost:8080/auth/register",
+                                        {username:"reactUser", password:"password"});
+    }.then(() => {
+        alert("Registration successful!");
+    })
 
 
     return(
@@ -27,6 +42,7 @@ export const Register:React.FC = () => {
 
                 <div>
                     {/*TODO: buttons*/}
+                    <Button onClick={register}>Create Account </Button>
                 </div>
             </div>
         </Container>
