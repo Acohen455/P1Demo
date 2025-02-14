af28d9bfc4f31a93d6bed68dae97f2c7ca23187c
Acohen455 <aaroncohen455@gmail.com>
2025-02-14 09:23:17 -0700

    Create AuthController.java

---
+++ P1DemoBackend/src/main/java/com/revature/Controllers/AuthController.java
@@ -0,0 +1,10 @@
+package com.revature.Controllers;
+
+//AuthController? Think AUTHentication/AUTHorization
+//I like to put account registration and login functionality here
+
+//What annotations do we need?
+@RestController //combines @Controller and @ResponseBody (makes a class a bean, and lets us send JSON responses)
+public class AuthController {
+    
+}
