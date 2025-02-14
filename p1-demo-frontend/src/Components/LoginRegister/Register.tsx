dc6295c0e2301b18f829cab1097ced72c32494f9
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-14 15:27:08 -0500

    working register maybe?

--- p1-demo-frontend/src/Components/LoginRegister/Register.tsx
+++ p1-demo-frontend/src/Components/LoginRegister/Register.tsx
@@ -1,7 +1,26 @@
-import { Container, Form } from "react-bootstrap"
+import axios from "axios"
+import { Button, Container, Form } from "react-bootstrap"
 
 export const Register:React.FC = () => {
 
+    //TODO: gather user input like with our hypotenuse calculator
+
+    //Hardcoding a new user registration with axios 
+    //axios is a way to send HTTP requests from React
+    const register = async () => {
+
+        //POST request with hardcoded user info
+        const response = await axios.post("http://localhost:8080/auth/register", {
+            username:"reactUser",
+            password:"password"
+        })
+        .then(() => {
+            alert("User created!")
+            //TODO: actually use the returned data
+        })
+
+
+    }
 
     return(
 
@@ -25,7 +44,7 @@ export const Register:React.FC = () => {
               </div>
 
               <div>
-                {/*TODO: buttons*/}
+                <Button onClick={register}>Create Account!</Button>
               </div>
           </div>
       </Container>
