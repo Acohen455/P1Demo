baa898dd5f84fd3ee05e5e6f627839664d2dce49
Acohen455 <aaroncohen455@gmail.com>
2025-02-14 11:23:31 -0700

    Updates

--- P1DemoBackend/src/main/java/com/revature/Controllers/AuthController.java
+++ P1DemoBackend/src/main/java/com/revature/Controllers/AuthController.java
@@ -3,6 +3,13 @@ package com.revature.Controllers;
 //AuthController? Think AUTHentication/AUTHorization
 //I like to put account registration and login functionality here
 
+import com.revature.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 //What annotations do we need?
 @RestController //combines @Controller and @ResponseBody (makes a class a bean, and lets us send JSON responses)
 @RequestMapping("/auth") //this indicates that HTTP requests to the listed address land on this controller
