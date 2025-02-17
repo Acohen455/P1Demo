4666db01eb2c79a532e66ea11ac2c32ad8173adc
Acohen455 <aaroncohen455@gmail.com>
2025-02-17 11:49:51 -0700

    Controller Updates

--- P1DemoBackend/src/main/java/com/revature/controllers/UserController.java
+++ P1DemoBackend/src/main/java/com/revature/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.revature.models.User;
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +20,13 @@ public class UserController {
 
     @Autowired
     public UserController(UserService userService) {
+
         this.userService = userService;
     }
 
 
     //return all users to the client
+    @GetMapping
     public ResponseEntity<List<OutgoingUserDTO>> getAllUsers() {
         //lets return the users in a one liner
         //the parameter to .ok() is the RESPONSE BODY
