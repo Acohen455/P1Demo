5850ee3b63bb45e9f2890e2c2684db35cb5a270e
Acohen455 <aaroncohen455@gmail.com>
2025-02-20 11:15:30 -0700

    adsf

---
+++ P1DemoBackend/src/main/java/com/revature/aspects/AuthAspect.java
@@ -0,0 +1,45 @@
+package com.revature.aspects;
+
+import jakarta.servlet.http.HttpSession;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect //This class is an ASPECT -- a class that can trigger functionality at any point in our app runtime
+//When a certain method is called, this class can listen for that invocation and trigger some functionality
+//Low overhead because it listens for a specific thing to happen, instead of a category of things
+@Component
+public class AuthAspect {
+
+    //2 use cases we'll see
+    //1) When any method in a controller OTHER than AuthController is called, make sure the user is logged in
+    //2) When any method that's annotated with our custom @AdminOnly annotation is called,
+    //...make sure the user is an admin
+
+    //@Before allows us to invoke this method BEFORE any method we specify
+    //This says
+    //Invoked the login check before any method in the controllers package BESIDES the AuthController
+    @Before("within(com.revature.controllers.*)" +
+            "&& !within(com.revature.controllers.AuthController)")
+    public void checkLoggedIn(){
+
+        //get access to the session (or lack thereof)
+        //we start by yoinking the attributes of the session context
+        ServletRequestAttributes attributes =
+                    (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpSession session = attributes.getRequest().getSession(false); //false means don't create a new session if one doesn't exist
+
+        //we're getting an object that has the session, and then trying to extract that session
+        //getSession(false) means a session wont get created if it doesnt exist
+
+        //if the session is null, the user is not logged in and we can throw an exception
+        if(session == null || session.getAttribute("userId") == null) {
+            throw new IllegalArgumentException("User must be logged in to do this!");
+        }
+
+    }
+
+
+}
--- P1DemoBackend/src/main/java/com/revature/exceptions/GlobalExceptionHandler.java
+++ P1DemoBackend/src/main/java/com/revature/exceptions/GlobalExceptionHandler.java
@@ -7,5 +7,27 @@ package com.revature.exceptions;
 //  OR: have clean controllers and handle all exception stuff here?
 //probably the second option
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice //This lets our handler intercept exceptions thrown in any controller
 public class GlobalExceptionHandler {
+    //In here, we can define how we want to handle any Exception that could possibly be thrown
+
+    //Right now we only have illegalargumentexception being thrown
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        //bad request is 400 status code
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+
+        //401 == UNAUTHORIZED - good for auth errors
+        //we send the message in the exception in the response body (should change based oin what went wrong)
+        //TODO: add more exception handlers, make some custom exceptions
+        //TODO: add a catchall handler for "Exception"
+    }
+
 }
